a920b0dc722460cb34134579f3644872ca48f149
Durval Moreira <durval.moreira@infracommerce.com.br>
2018-04-24 23:25:50 -0300

    add router

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom';
 
 const title = 'My Minimal React Webpack Babel Setup';
+const Home = () => (
+    <div>
+        <h3>Home</h3>
+    </div>
+)
+const Users = ({ id }) => (
+    <div>
+        <h3>Users</h3>
+    </div>
+)
+
+const Menu = () => (
+    <Router>
+        <div>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/users">Users</Link></li>
+            </ul>
+            <hr />
+
+            <Route exact path="/" component={Home} />
+            <Route path="/users" component={Users} />
+        </div>
+    </Router>
+)
 
 ReactDOM.render(
-    <div className="teste-class">{title}</div>,
+    <div className="teste-class">
+        <Menu />
+    </div>,
     document.getElementById('app')
 );
